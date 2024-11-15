3d52f7e2247f78e2bdb75c04afe537ade26489c3
Makoto So <m.sou@okiu.ac.jp>
2024-11-15 21:05:41 +0900

    ad3
    あそびのデザイン講座3

---
+++ Assets/Scripts/Broken.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Broken : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the object collided with has the tag "Ball"
+        if (collision.gameObject.tag == "Player")
+        {
+            // Destroy this game object
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
